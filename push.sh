8f6fe8886d203ea134ca74ad0a035a60e0e62bf6
Pablo Salomone <lsalomone@ghgsat.com>
2025-04-03 10:16:18 -0400

    add Howto Folder

---
+++ push.sh
@@ -0,0 +1,7 @@
+#Push To GitHub
+git add  .
+git commit -m "add Howto Folder"
+#git commit -m "New files added "
+#git remote add origin https://github.com/r00tz0devops/obsidian.git
+git branch -M main
+git push -u origin main
